a6873d40f96007238e8ded64c9250b885422f10f
TahaEZ <taha13800111@gmail.com>
2023-02-25 23:23:45 +0330

    parse user input and select a command accordingly

--- src/main/java/org/iespring1402/Main.java
+++ src/main/java/org/iespring1402/Main.java
@@ -1,19 +1,7 @@
 package org.iespring1402;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.iespring1402.response.FailedResponse;
-import org.iespring1402.response.Response;
-import org.iespring1402.response.SuccessfulResponse;
-
 public class Main {
-    public static void main(String[] args) {
-        Response response = new FailedResponse();
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            String stringResponse = mapper.writeValueAsString(response);
-            System.out.println(stringResponse);
-        } catch (Exception e){
-            System.out.println(e);
-        }
+    public static void main(String[] args){
+        CommandLineInterface.main(args);
     }
 }
