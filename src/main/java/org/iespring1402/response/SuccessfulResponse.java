b9da531fb7a402c160443d6169388326daf6d596
TahaEZ <taha13800111@gmail.com>
2023-02-26 11:50:15 +0330

    Remove CommandLineInterface class and add exit command

--- src/main/java/org/iespring1402/response/SuccessfulResponse.java
+++ src/main/java/org/iespring1402/response/SuccessfulResponse.java
@@ -1,6 +1,6 @@
 package org.iespring1402.response;
 
-public class SuccessfulResponse extends Response{
+public class SuccessfulResponse extends Response {
 
     public SuccessfulResponse() {
         super(true, "Your request was successfully done");
