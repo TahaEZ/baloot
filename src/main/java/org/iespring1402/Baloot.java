e4c58a4c7b153dcf8881215c45a6a72d0a7e8a4e
Aliw7979 <ali.black.viper@gmail.com>
2023-02-26 17:41:09 +0330

    CommodityContainer was added.

--- src/main/java/org/iespring1402/Baloot.java
+++ src/main/java/org/iespring1402/Baloot.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Baloot {
     ArrayList<User> users;
+    ArrayList<Commodity> commodities;
 
     public Baloot() {
         this.users = new ArrayList<User>();
@@ -16,5 +17,10 @@ public class Baloot {
     public void addUser(String username, String password, String email, String birthDate, String address, long credit) {
         User newUser = new User(username, password, email, birthDate, address, credit);
         users.add(newUser);
+    }
+
+    public  void addCommodity(int id, String name, int providerId , int price, ArrayList<String> categories, float rating, int inStock) {
+        Commodity newCommodity = new Commodity(id, name, providerId , price, categories, rating,inStock);
+        commodities.add(newCommodity);
     }
 }
