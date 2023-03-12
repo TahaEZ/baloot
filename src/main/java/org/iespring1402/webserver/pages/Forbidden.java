2fddf9e7bb57d3bf24557f023679967363588e4f
TahaEZ <taha13800111@gmail.com>
2023-03-12 18:40:08 +0330

    Add Forbidden page

---
+++ src/main/java/org/iespring1402/webserver/pages/Forbidden.java
@@ -0,0 +1,16 @@
+package org.iespring1402.webserver.pages;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Forbidden extends Page {
+    public static String result() throws IOException {
+        String dir = System.getProperty("user.dir");
+        File forbiddenFile = new File(dir + "/src/main/java/org/iespring1402/webserver/pages/templates/403.html");
+        Document forbiddenDocument = Jsoup.parse(forbiddenFile, "UTF-8");
+        return forbiddenDocument.outerHtml();
+    }
+}
