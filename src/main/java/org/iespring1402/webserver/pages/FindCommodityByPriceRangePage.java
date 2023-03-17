5b63d2bb5943d9da59c2faace2dd39b038c4bcc9
Aliw7979 <ali.black.viper@gmail.com>
2023-03-17 19:24:47 +0330

    Test Completed Successfully

--- src/main/java/org/iespring1402/webserver/pages/FindCommodityByPriceRangePage.java
+++ src/main/java/org/iespring1402/webserver/pages/FindCommodityByPriceRangePage.java
@@ -1,7 +1,6 @@
 package org.iespring1402.webserver.pages;
 
 import org.iespring1402.Baloot;
-import org.iespring1402.CategoryFilter;
 import org.iespring1402.Commodity;
 import org.jsoup.nodes.Document;
 
@@ -9,9 +8,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class FindCommodityByPriceRangePage {
+
+    private static ArrayList<Commodity> filteredByPriceRange = new ArrayList<>();
     public static String result(long startPrice , long endPrice ) throws IOException {
+
+
+        if(startPrice > endPrice || startPrice < 0) {return ForbiddenPage.result();}
+
+        filteredByPriceRange.clear();
+
         ArrayList<Commodity> commodities = Baloot.getInstance().getCommodities();
-        ArrayList<Commodity> filteredByPriceRange = new ArrayList<>();
         for(Commodity commodity : commodities)
         {
             if(commodity.getPrice() >= startPrice && commodity.getPrice() <= endPrice )
@@ -22,5 +28,9 @@ public class FindCommodityByPriceRangePage {
 
         Document commoditiesByPriceRangeDocument = CommoditiesPage.createCommoditiesDocument(filteredByPriceRange);
         return commoditiesByPriceRangeDocument.outerHtml();
+    }
+
+    public static ArrayList<Commodity> getFilteredByPriceRange() {
+        return filteredByPriceRange;
     }
 }
--- src/test/java/WebServerTest.java
+++ src/test/java/WebServerTest.java
@@ -1,7 +1,9 @@
 import org.iespring1402.Baloot;
 
 import org.iespring1402.BuyList;
+import org.iespring1402.Commodity;
 import org.iespring1402.webserver.pages.AddToBuyListPage;
+import org.iespring1402.webserver.pages.FindCommodityByPriceRangePage;
 import org.iespring1402.webserver.pages.RemoveFromBuyListPage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -18,39 +21,77 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WebServerTest {
 
-   private Baloot baloot;
+    private Baloot baloot;
+
     @BeforeEach
     void setup() {
         baloot = Baloot.getInstance();
     }
 
     @AfterEach
-    public void teardown(){
+    public void teardown() {
         Baloot.removeInstance();
     }
 
-
-
     @Test
-    public  void testBuyList() {
+    public void testBuyList() {
         String username = "ali";
         int[] commodityIdToAdd = {1, 2};
         try {
-            AddToBuyListPage.result(username,commodityIdToAdd[0]);
-            AddToBuyListPage.result(username,commodityIdToAdd[1]);
-            BuyList buyList =baloot.findUserByUsername("ali").getBuyList();
-            ArrayList <Integer> commodityId = buyList.getList();
+            AddToBuyListPage.result(username, commodityIdToAdd[0]);
+            AddToBuyListPage.result(username, commodityIdToAdd[1]);
+            BuyList buyList = baloot.findUserByUsername("ali").getBuyList();
+            ArrayList<Integer> commodityId = buyList.getList();
             assertEquals(1, commodityId.get(0));
             assertEquals(2, commodityId.get(1));
-            RemoveFromBuyListPage.result(username,commodityIdToAdd[0]);
+            RemoveFromBuyListPage.result(username, commodityIdToAdd[0]);
             assertEquals(2, commodityId.get(0));
-            RemoveFromBuyListPage.result(username,commodityIdToAdd[1]);
-            assertEquals(0,commodityId.size());
-        }
-        catch (IOException e)
-        {
-            assertEquals(IOException.class,e.getClass());
+            RemoveFromBuyListPage.result(username, commodityIdToAdd[1]);
+            assertEquals(0, commodityId.size());
+        } catch (IOException e) {
+            assertEquals(IOException.class, e.getClass());
         }
     }
+
+    private void callPriceRangeFilter(FindCommodityByPriceRangePage priceRangeFilter, int startPrice, int endPrice) {
+        try {
+            priceRangeFilter.result(startPrice, endPrice);
+        } catch (IOException e) {
+            assertEquals(false, true);
+        }
+    }
+
+    @Test
+    public void testCommodityByPriceRange() {
+
+        int startPrice = 1000;
+        int endPrice = 50000;
+        FindCommodityByPriceRangePage priceRangeFilter = new FindCommodityByPriceRangePage();
+        callPriceRangeFilter(priceRangeFilter, startPrice, endPrice);
+        ArrayList<Commodity> filtered = priceRangeFilter.getFilteredByPriceRange();
+        for (Commodity commodity : filtered) {
+            assertEquals(true, commodity.getPrice() >= startPrice);
+            assertEquals(true, commodity.getPrice() <= endPrice);
+        }
+        startPrice = 14000;
+        endPrice = 14000;
+        callPriceRangeFilter(priceRangeFilter, startPrice, endPrice);
+        for (Commodity commodity : filtered) {
+            assertEquals(true, commodity.getPrice() >= startPrice);
+            assertEquals(true, commodity.getPrice() <= endPrice);
+        }
+        startPrice = 0;
+        endPrice = 0;
+        callPriceRangeFilter(priceRangeFilter, startPrice, endPrice);
+
+        assertEquals(0, filtered.size());
+
+        startPrice = -1;
+        endPrice = 1000;
+        callPriceRangeFilter(priceRangeFilter, startPrice, endPrice);
+
+        assertEquals(0, filtered.size());
+    }
+
 
 }
