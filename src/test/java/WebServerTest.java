49de7d13b5be03cec438b5dcd8f40ba712c368e8
TahaEZ <taha13800111@gmail.com>
2023-03-17 21:00:54 +0330

    remove unwanted imports

--- src/test/java/WebServerTest.java
+++ src/test/java/WebServerTest.java
@@ -12,12 +12,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class WebServerTest {
