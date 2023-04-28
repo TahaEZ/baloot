6fde5c105b20c8c6df27cfe06926553643e4f57d
Aliw7979 <ali.black.viper@gmail.com>
2023-04-29 00:48:19 +0330

    Added login , commodity and rate api

--- src/main/java/org/iespring1402/Baloot/controller/commodityController.java
+++ src/main/java/org/iespring1402/Baloot/controller/commodityController.java
@@ -7,14 +7,17 @@ import org.iespring1402.Baloot.model.Baloot;
 import org.iespring1402.Baloot.model.Commodity;
 import org.iespring1402.Baloot.model.CommodityByIdView;
 import org.iespring1402.Baloot.model.Provider;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/commodity")
@@ -30,8 +33,12 @@ public class commodityController {
     }
 
     @GetMapping(value = "")
-    public Object getMethodName(@RequestParam int id) {
+    @ResponseBody
+    public Object getCommodityById(@RequestParam int id) {
         Commodity commodity = balootInstance.findCommodityById(id);
+        if (commodity == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Commodity Not Found.");
+        }
         CommodityByIdView result;
         Provider provider = balootInstance.findProviderByProviderId(commodity.getProviderId());
         result = new CommodityByIdView(commodity.getId(), commodity.getName(), provider.getName(),
@@ -39,4 +46,23 @@ public class commodityController {
         return result;
     }
 
+    @PostMapping(value = "/rate")
+    public Object rateCommodity(@RequestBody String username, int id, int rate) {
+
+        if (rate >= 1 && rate <= 10) {
+            try {
+                balootInstance.rateCommodity(username, id, rate);
+
+            } catch (Exception e) {
+
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        }
+
+        else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+    }
 }
---
+++ src/main/java/org/iespring1402/Baloot/controller/loginController.java
@@ -0,0 +1,32 @@
+package org.iespring1402.Baloot.controller;
+import org.iespring1402.Baloot.model.Baloot;
+import org.iespring1402.Baloot.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/login")
+@CrossOrigin
+public class loginController {
+    private Baloot balootInstance = new Baloot();
+    @GetMapping(value = "")
+    @ResponseBody
+    public Object credentialsCheck(@RequestParam String username , String password) {
+        User user = balootInstance.findUserByUsername(username);
+        if (user == null || user.password != password) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        else
+        {
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
+
+        }
+    }
+
+}
