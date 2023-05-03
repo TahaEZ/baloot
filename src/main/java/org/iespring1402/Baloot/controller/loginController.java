961c17266991c3dd7fcd8d89d51469b5d4eeec9c
Aliw7979 <ali.black.viper@gmail.com>
2023-05-03 19:39:58 +0330

    fix on login error!

--- src/main/java/org/iespring1402/Baloot/controller/loginController.java
+++ src/main/java/org/iespring1402/Baloot/controller/loginController.java
@@ -9,20 +9,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/login")
 @CrossOrigin
-public class loginController {
+public class loginController{
+    
     private Baloot balootInstance = new Baloot();
 
     @GetMapping(value = "")
     @ResponseBody
-    public Object credentialsCheck(@RequestParam String username, String password) {
+    public Object credentialsCheck(@RequestParam String username, String password) throws Exception {
         User user = balootInstance.findUserByUsername(username);
         if (user == null || !user.getPassword().equals(password)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid username or password!");
+            ObjectMapper mapper = new ObjectMapper();
+            String body;
+            body=mapper.writeValueAsString("Invalid username or password!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
         } else {
             return ResponseEntity.status(HttpStatus.OK).body(user);
         }
