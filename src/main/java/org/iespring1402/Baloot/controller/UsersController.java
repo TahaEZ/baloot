7c45905d9dfc267952b86e54c99e5d14acb7250d
Aliw7979 <ali.black.viper@gmail.com>
2023-07-03 16:46:13 +0330

    userController Completed without test.

--- src/main/java/org/iespring1402/Baloot/controller/UsersController.java
+++ src/main/java/org/iespring1402/Baloot/controller/UsersController.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.iespring1402.Baloot.models.Baloot;
+import org.iespring1402.Baloot.entities.BuyList;
 import org.iespring1402.Baloot.entities.Commodity;
-import org.iespring1402.Baloot.models.User;
+import org.iespring1402.Baloot.entities.User;
 import org.iespring1402.Baloot.models.views.CommodityDTO;
+import org.iespring1402.Baloot.repositories.CommodityDAO;
 import org.iespring1402.Baloot.repositories.UserDAO;
 import org.iespring1402.Baloot.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +34,9 @@ public class UsersController {
     @Autowired
     private UserDAO userDAO;
 
+    @Autowired 
+    private CommodityDAO commodityDAO;
+
     @GetMapping(value = "/{username}")
     @ResponseBody
     public Object getUserByUsername(@PathVariable("username") String username, @RequestAttribute boolean unauthorized) {
@@ -39,7 +44,8 @@ public class UsersController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing authorization");
         }
 
-        User user = balootInstance.findUserByUsername(username);
+        User user = userDAO.getUserByUsername(username);
+        
         if (user == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         } else {
@@ -56,13 +62,13 @@ public class UsersController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing authorization");
         }
 
-        User user = balootInstance.findUserByUsername(username);
+        User user = userDAO.getUserByUsername(username);
         HashMap<String, Object> buylist = new HashMap<>();
         if (user == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         } else {
-            buylist.put("buylist", balootInstance.getBuyList(username));
-            buylist.put("totalCost", balootInstance.findUserByUsername(username).getBuyList().totalCost());
+            buylist.put("buylist", user.getBuyList());
+            buylist.put("totalCost", user.getBuyList().totalCost());
             return buylist;
         }
     }
@@ -76,12 +82,13 @@ public class UsersController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing authorization");
         }
 
-        User user = balootInstance.findUserByUsername(username);
+        User user = userDAO.getUserByUsername(username);
         HashMap<String, Object> response = new HashMap<>();
         if (user == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         } else {
-            Commodity commodity = balootInstance.findCommodityById(commodityId);
+            Commodity commodity = commodityDAO.findCommodityById(commodityId);
+            
             if (commodity == null) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Commodity not found!");
             } else if (commodity.getInStock() == 0) {
@@ -89,15 +96,31 @@ public class UsersController {
             } else {
                 Response responseStatus = user.addToBuyList(commodityId);
                 if (responseStatus.success) {
-                    ArrayList<CommodityDTO> buyList = balootInstance.getBuyList(username);
+                    userDAO.save(user);
+                    ArrayList<CommodityDTO> buyList = getBuyListAsList(userDAO.getUserByUsername(username).getBuyList());
                     response.put("buylist", buyList);
-                    response.put("totalCost", balootInstance.findUserByUsername(username).getBuyList().totalCost());
+                    response.put("totalCost", userDAO.getUserByUsername(username).getBuyList().totalCost());
                     return response;
                 } else {
                     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Internal Error!");
                 }
             }
         }
+    }
+    
+    public ArrayList<CommodityDTO> getBuyListAsList(BuyList buyList) {
+        HashMap<Integer, Integer> buylist = buyList.getItems();
+        ArrayList<CommodityDTO> result = new ArrayList<>();
+        for (HashMap.Entry<Integer, Integer> item : buylist.entrySet()) {
+            Commodity commodity = commodityDAO.findCommodityById(item.getKey());
+
+            CommodityDTO buylistItem = new CommodityDTO(commodity.getId(), commodity.getName(),
+                    commodity.getProviderId(), commodity.getPrice(), commodity.getCategories(),
+                    commodity.getRating(), commodity.getInStock(), item.getValue(), commodity.getImage());
+
+            result.add(buylistItem);
+        }
+        return result;
     }
 
     @DeleteMapping(value = "/{username}/buyList")
@@ -110,18 +133,19 @@ public class UsersController {
         }
 
         HashMap<String, Object> response = new HashMap<>();
-        User user = balootInstance.findUserByUsername(username);
+        User user = userDAO.getUserByUsername(username);
         if (user == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         } else {
-            Commodity commodity = balootInstance.findCommodityById(commodityId);
+            Commodity commodity = commodityDAO.findCommodityById(commodityId);
             if (commodity == null) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Commodity not found!");
             } else {
                 Response responseStatus = user.removeFromBuyList(commodityId);
                 if (responseStatus.success) {
-                    ArrayList<CommodityDTO> buyList = balootInstance.getBuyList(username);
+                    ArrayList<CommodityDTO> buyList = getBuyListAsList(userDAO.getUserByUsername(username).getBuyList());
                     response.put("buylist", buyList);
+                    userDAO.save(user);
                     return ResponseEntity.status(HttpStatus.OK).body(response);
                 } else {
                     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Item Not found in buylist!");
@@ -139,13 +163,13 @@ public class UsersController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing authorization");
         }
 
-        User user = balootInstance.findUserByUsername(username);
+        User user = userDAO.getUserByUsername(username);
         HashMap<String, Object> purchasedList = new HashMap<>();
         if (user == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         } else {
             purchasedList.put("purchasedList",
-                    balootInstance.findUserByUsername(username).getPurchasedList().getPurchasedItems());
+                    userDAO.getUserByUsername(username).getPurchasedItems());
             return purchasedList;
         }
     }
@@ -159,12 +183,13 @@ public class UsersController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing authorization");
         }
 
-        User user = balootInstance.findUserByUsername(username);
+        User user = userDAO.getUserByUsername(username);
         if (user == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         } else {
             Response addCreditRes = user.addCredit(creditToAdd);
             if (addCreditRes.success) {
+                userDAO.save(user);
                 return ResponseEntity.status(HttpStatus.OK).body(addCreditRes.data);
             } else {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(addCreditRes.data);
